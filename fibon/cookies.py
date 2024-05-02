44ba0e877f2b6c63410d9ea2ff33851ce24640ff
Alex <alexgw24@googlemail.com>
2024-05-02 22:07:21 +0100

    cookies and algo version

--- fibon/cookies.py
+++ fibon/cookies.py
@@ -7,24 +7,6 @@ PORT = 8080
 #prev = 0
 
 
-def getfib(n):
-    current = 0
-    prev = 0
-    next = 0
-    i = 0; 
-    while i < n:
-        next = prev + current
-        #print("current: ", current, "prev: ", prev, "next: ", next)
-        #print(next)
-        prev = current
-        if current == 0:
-            next = 1
-        current = next
-        i += 1
-    
-    return next
-
-
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -35,22 +17,37 @@ class Handler(BaseHTTPRequestHandler):
             C = cookies.SimpleCookie(self.headers.get('Cookie')) 
             print(C.keys()) 
             #setCookies
-            if "count" not in C.keys():
-                print("set initial count to 0")
-                C["count"] = 0
+            if 'prev' not in C.keys() :
+                print("setting initial prev")
+                C["prev"] = 1
+                prev = 1
             else:
-                C["count"] = int(C["count"].value) + 1
+                prev = int(C["prev"].value)
+
+            if 'current' not in C.keys():
+                print("setting initial current")
+                C["current"] = 0
+                current = 0
+            else:
+                current = int(C["current"].value)
+
+            next = prev + current
+
+            
+            C["prev"] = current
+            C["current"] = next
+            C["test"] = "end"
 
             for morsel in C.values():
                 self.send_header("Set-Cookie", morsel.OutputString())
-            
-            count = int(C["count"].value)
+            print(prev, current, C)
+
 
             self.end_headers()
             #set the current number to the global number
             #the next number in the sequence is the prev + current
-            print(count)
-            content = '<html><body style="background: #202020"><p style="color:antiquewhite; margin: auto; padding: 20% 20%; font-size: 10vw;; ">' +  str(getfib(count)) + '</p></body></html>' 
+            print(next)
+            content = '<html><body style="background: #202020"><p style="color:antiquewhite; margin: auto; padding: 20% 20%; font-size: 20rem; ">' +  str(current) + '</p></body></html>' 
             content = content.encode("UTF8")
             self.wfile.write(content)
             
--- fibon/todo.md
+++ fibon/todo.md
@@ -1,5 +1,11 @@
 # Fibonacci Toy
 
-- [x] Simple per request state on server
-- [ ] Per client state in cookies
+- [x] Simple per request state on server (first.py)
+- [x] Per client state in cookies (cookies.py)
+- [x] algo version with count cookie (main.py)
 - [ ] Fibonacci algorithm for arbitrary reqs
+
+
+per client state could do with some more work as I think it repeats 1 twice. 
+
+Does the algorithm have to be recursive? 
